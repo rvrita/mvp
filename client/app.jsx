f3bf7629cf139ff91031e10f9a3fafeb7995d167
Rita Rozsavolgyi <rvrita.me@gmail.com>
2020-07-11 19:48:14 -0700

    Basic board rendering

--- client/app.jsx
+++ client/app.jsx
@@ -1,1 +1,75 @@
-ReactDOM.render(<h1>Hello from react!</h1>, document.getElementById('app'));
+
+class Card extends React.Component {
+  render() {
+    return (
+      <td className="card">{this.props.value}</td>
+    )
+  }
+}
+
+class Board extends React.Component {
+  render() {
+    return (
+      <table className="board">
+        <tbody>
+          {this.props.words.map((row, indexRow) => {
+            return (
+              <tr key={indexRow}>
+                {row.map((card, indexCol) => <Card value={card} key={indexRow * 6 + indexCol} />)}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    )
+  }
+}
+
+class InfoBoard extends React.Component {
+  render() {
+    return (
+      <div className="infoboard">
+        <div className="player">Team {this.props.isBluesTurn ? 'Blue' : 'Red'}'s turn</div>
+        <button className="revealbutton">Reveal</button>
+      </div>
+    )
+  }
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isBluesTurn: true,
+      words: [
+        ['ball', 'fish', 'track', 'maple', 'pole'],
+        ['sink', 'apple', 'spell', 'kid', 'cab'],
+        ['lemon', 'scientist', 'whip', 'coffee', 'cat'],
+        ['leaf', 'dance', 'princess', 'net', 'Europe'],
+        ['water', 'yellow', 'bridge', 'watermelon', 'tea']
+      ],
+      colors: [
+        ['blue', 'blue', 'red', 'yellow', 'yellow'],
+        ['red', 'black', 'blue', 'red', 'blue'],
+        ['red', 'blue', 'red', 'yellow', 'yellow'],
+        ['blue', 'blue', 'red', 'blue', 'yellow'],
+        ['yellow', 'yellow', 'red', 'blue', 'red']
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Codenames</h1>
+        </div>
+        <Board words={this.state.words} />
+        <InfoBoard isBluesTurn={this.state.isBluesTurn} />
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(<Game />, document.getElementById('app'));
--- public/app.js
+++ public/app.js
@@ -1,6 +1,142 @@
-ReactDOM.render(React.createElement(
-  'h1',
-  null,
-  'Hello from react!'
-), document.getElementById('app'));
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Card = function (_React$Component) {
+  _inherits(Card, _React$Component);
+
+  function Card() {
+    _classCallCheck(this, Card);
+
+    return _possibleConstructorReturn(this, (Card.__proto__ || Object.getPrototypeOf(Card)).apply(this, arguments));
+  }
+
+  _createClass(Card, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "td",
+        { className: "card" },
+        this.props.value
+      );
+    }
+  }]);
+
+  return Card;
+}(React.Component);
+
+var Board = function (_React$Component2) {
+  _inherits(Board, _React$Component2);
+
+  function Board() {
+    _classCallCheck(this, Board);
+
+    return _possibleConstructorReturn(this, (Board.__proto__ || Object.getPrototypeOf(Board)).apply(this, arguments));
+  }
+
+  _createClass(Board, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "table",
+        { className: "board" },
+        React.createElement(
+          "tbody",
+          null,
+          this.props.words.map(function (row, indexRow) {
+            return React.createElement(
+              "tr",
+              { key: indexRow },
+              row.map(function (card, indexCol) {
+                return React.createElement(Card, { value: card, key: indexRow * 6 + indexCol });
+              })
+            );
+          })
+        )
+      );
+    }
+  }]);
+
+  return Board;
+}(React.Component);
+
+var InfoBoard = function (_React$Component3) {
+  _inherits(InfoBoard, _React$Component3);
+
+  function InfoBoard() {
+    _classCallCheck(this, InfoBoard);
+
+    return _possibleConstructorReturn(this, (InfoBoard.__proto__ || Object.getPrototypeOf(InfoBoard)).apply(this, arguments));
+  }
+
+  _createClass(InfoBoard, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        { className: "infoboard" },
+        React.createElement(
+          "div",
+          { className: "player" },
+          "Team ",
+          this.props.isBluesTurn ? 'Blue' : 'Red',
+          "'s turn"
+        ),
+        React.createElement(
+          "button",
+          { className: "revealbutton" },
+          "Reveal"
+        )
+      );
+    }
+  }]);
+
+  return InfoBoard;
+}(React.Component);
+
+var Game = function (_React$Component4) {
+  _inherits(Game, _React$Component4);
+
+  function Game(props) {
+    _classCallCheck(this, Game);
+
+    var _this4 = _possibleConstructorReturn(this, (Game.__proto__ || Object.getPrototypeOf(Game)).call(this, props));
+
+    _this4.state = {
+      isBluesTurn: true,
+      words: [['ball', 'fish', 'track', 'maple', 'pole'], ['sink', 'apple', 'spell', 'kid', 'cab'], ['lemon', 'scientist', 'whip', 'coffee', 'cat'], ['leaf', 'dance', 'princess', 'net', 'Europe'], ['water', 'yellow', 'bridge', 'watermelon', 'tea']],
+      colors: [['blue', 'blue', 'red', 'yellow', 'yellow'], ['red', 'black', 'blue', 'red', 'blue'], ['red', 'blue', 'red', 'yellow', 'yellow'], ['blue', 'blue', 'red', 'blue', 'yellow'], ['yellow', 'yellow', 'red', 'blue', 'red']]
+    };
+    return _this4;
+  }
+
+  _createClass(Game, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        React.createElement(
+          "div",
+          null,
+          React.createElement(
+            "h1",
+            null,
+            "Codenames"
+          )
+        ),
+        React.createElement(Board, { words: this.state.words }),
+        React.createElement(InfoBoard, { isBluesTurn: this.state.isBluesTurn })
+      );
+    }
+  }]);
+
+  return Game;
+}(React.Component);
+
+ReactDOM.render(React.createElement(Game, null), document.getElementById('app'));
 //# sourceMappingURL=app.js.map
