bf1273861f366a287902bfd86497e4d603bd751a
Rita Rozsavolgyi <rvrita.me@gmail.com>
2020-07-12 21:50:43 -0700

    Basic reveal/show card colors logic, WIP scoreboard

--- client/app.jsx
+++ client/app.jsx
@@ -1,13 +1,18 @@
-
 class Card extends React.Component {
   render() {
     var classes = 'card';
-    console.log('in card', this.props.isRevealed)
-    if (this.props.isRevealed) {
-      classes += ` ${this.props.color}b`;
+    if (this.props.isRevealButtonOn) {
+      classes += ` ${this.props.card.color}b`;
+    }
+    if (this.props.card.isGuessed) {
+      classes += ` ${this.props.card.color}`;
     }
     return (
-      <td className={classes}>{this.props.value}</td>
+      <td className={classes} onClick={(e) => {
+        e.preventDefault();
+        this.props.handleCardClick(this.props.indexRow, this.props.indexCol)
+      }
+      }>{this.props.card.word}</td>
     )
   }
 }
@@ -17,13 +22,16 @@ class Board extends React.Component {
     return (
       <table className="board">
         <tbody>
-          {this.props.words.map((row, indexRow) => {
+          {this.props.cards.map((row, indexRow) => {
             return (
               <tr key={indexRow}>
                 {row.map((card, indexCol) => <Card
-                  value={card}
-                  key={indexRow * 6 + indexCol}
-                  isRevealed={this.props.isRevealed} color={this.props.colors[indexRow][indexCol]} />)}
+                  card={card}
+                  key={indexRow * 5 + indexCol}
+                  isRevealButtonOn={this.props.isRevealButtonOn}
+                  indexRow={indexRow}
+                  indexCol={indexCol}
+                  handleCardClick={this.props.handleCardClick} />)}
               </tr>
             )
           })}
@@ -36,12 +44,83 @@ class Board extends React.Component {
 class InfoBoard extends React.Component {
   render() {
     return (
+      <div className="flexcontainer">
+        <Player team='Blue' />
+        <Info
+          isBluesTurn={this.props.isBluesTurn}
+          handleRevealClick={this.props.handleRevealClick}
+          handleSkipClick={this.props.handleSkipClick}
+          handleRestartClick={this.props.handleRestartClick}/>
+        <Player team='Red' />
+      </div>
+    )
+  }
+}
+
+class Player extends React.Component {
+  render() {
+    return (
+        <div className="scoreboard">
+          <h3>{this.props.team} Team's Scores:</h3>
+          <table>
+            <tbody>
+              <tr>
+                <td>All cards:</td>
+                <td>9</td>
+              </tr>
+              <tr>
+                <td>Remaining:</td>
+                <td>4</td>
+              </tr>
+            </tbody>
+          </table>
+          {/* <p>Hints</p>
+          <ul>
+            <li>color</li>
+            <li>animal</li>
+          </ul>
+          <form>
+            <label for="hint">Add a hint:</label>
+            <input type="text" id="hint" name="hint"/><br/>
+            <label for="numberofwords">Number of words:</label>
+            <input type="text" id="numberofwords" name="numberofwords"/>
+            <input type="submit" value="Submit"/>
+          </form> */}
+        </div>
+    )
+  }
+}
+
+class Info extends React.Component {
+  render() {
+    return (
       <div className="infoboard">
-        <div className="player">Team {this.props.isBluesTurn ? 'Blue' : 'Red'}'s turn</div>
+        <h3 className="playerturn">Team {this.props.isBluesTurn ? 'Blue' : 'Red'}'s turn</h3>
+        <button className="skipbutton" onClick={(e) => {
+          e.preventDefault();
+          this.props.handleSkipClick();
+        }}>Skip</button><br />
         <button className="revealbutton" onClick={(e) => {
           e.preventDefault();
-          this.props.handleRevealClick()
-        }}>Reveal</button>
+          this.props.handleRevealClick();
+        }}>Reveal</button><br />
+        <button className="restartbutton" onClick={(e) => {
+          e.preventDefault();
+          this.props.handleRestartClick();
+        }}>Restart</button>
+      </div>
+    )
+  }
+}
+
+class Rules extends React.Component {
+  render() {
+    return (
+      <div className="rules">
+        <h4>Rules</h4>
+        <p>
+        Codenames is a game of guessing which codenames (i.e., words) in a set are related to a hint-word given by another player.<br/><br/>Players split into two teams: red and blue. One player of each team is selected as the team's spymaster; the others are field operatives.<br/><br/>Twenty-five Codename cards, each bearing a word, are laid out in a 5×5 rectangular grid, in random order. A number of these words represent red agents, a number represent blue agents, one represents an assassin, and the others represent innocent bystanders.<br/><br/>The teams' spymasters are given a randomly-dealt map card showing a 5×5 grid of 25 squares of various colors, each corresponding to one of the code name cards on the table. Teams take turns. On each turn, the appropriate spymaster gives a verbal hint about the words on the respective cards. Each hint may only consist of one single word and a number. The spymaster gives a hint that is related to as many of the words on his/her own agents' cards as possible, but not to any others – lest they accidentally lead their team to choose a card representing an innocent bystander, an opposing agent, or the assassin.<br/><br/>The hint's word can be chosen freely, as long as it is not (and does not contain) any of the words on the code name cards still showing at that time. Code name cards are covered as guesses are made.<br/><br/>The hint's number tells the field operatives how many words in the grid are related to the word of the clue. It also determines the maximum number of guesses the field operatives may make on that turn, which is the hint's number plus one. Field operatives must make at least one guess per turn, risking a wrong guess and its consequences. They may also end their turn voluntarily at any point thereafter.<br/><br/>After a spymaster gives the hint with its word and number, their field operatives make guesses about which code name cards bear words related to the hint and point them out, one at a time. When a code name card is pointed out, the spymaster covers that card with an appropriate identity card – a blue agent card, a red agent card, an innocent bystander card, or the assassin card – as indicated on the spymasters' map of the grid. If the assassin is pointed out, the game ends immediately, with the team who identified him losing. If an agent of the other team is pointed out, the turn ends immediately, and that other team is also one agent closer to winning. If an innocent bystander is pointed out, the turn simply ends.<br/><br/>The game ends when all of one team's agents are identified (winning the game for that team),[3] or when one team has identified the assassin (losing the game). Source: <a href="https://en.wikipedia.org/wiki/Codenames_(board_game)">Wikipedia</a>
+        </p>
       </div>
     )
   }
@@ -52,29 +131,65 @@ class Game extends React.Component {
     super(props);
     this.state = {
       isBluesTurn: true,
-      words: [
-        ['ball', 'fish', 'track', 'maple', 'pole'],
-        ['sink', 'apple', 'spell', 'kid', 'cab'],
-        ['lemon', 'scientist', 'whip', 'coffee', 'cat'],
-        ['leaf', 'dance', 'princess', 'net', 'Europe'],
-        ['water', 'yellow', 'bridge', 'watermelon', 'tea']
+      cards: [
+        [{ word: 'ball', color: 'blue', isGuessed: false },
+        { word: 'fish', color: 'blue', isGuessed: false },
+        { word: 'track', color: 'red', isGuessed: false },
+        { word: 'maple', color: 'yellow', isGuessed: false },
+        { word: 'pole', color: 'yellow', isGuessed: false }],
+        [{ word: 'sink', color: 'red', isGuessed: false },
+        { word: 'apple', color: 'black', isGuessed: false },
+        { word: 'spell', color: 'blue', isGuessed: false },
+        { word: 'kid', color: 'red', isGuessed: false },
+        { word: 'cab', color: 'blue', isGuessed: false }],
+        [{ word: 'lemon', color: 'red', isGuessed: false },
+        { word: 'scientist', color: 'blue', isGuessed: false },
+        { word: 'whip', color: 'red', isGuessed: false },
+        { word: 'coffee', color: 'yellow', isGuessed: false },
+        { word: 'cat', color: 'yellow', isGuessed: false }],
+        [{ word: 'leaf', color: 'blue', isGuessed: false },
+        { word: 'dance', color: 'blue', isGuessed: false },
+        { word: 'princess', color: 'red', isGuessed: false },
+        { word: 'net', color: 'blue', isGuessed: false },
+        { word: 'Europe', color: 'yellow', isGuessed: false }],
+        [{ word: 'water', color: 'yellow', isGuessed: false },
+        { word: 'yellow', color: 'yellow', isGuessed: false },
+        { word: 'bridge', color: 'red', isGuessed: false },
+        { word: 'watermelon', color: 'blue', isGuessed: false },
+        { word: 'tea', color: 'red', isGuessed: false }]
       ],
-      colors: [
-        ['blue', 'blue', 'red', 'yellow', 'yellow'],
-        ['red', 'black', 'blue', 'red', 'blue'],
-        ['red', 'blue', 'red', 'yellow', 'yellow'],
-        ['blue', 'blue', 'red', 'blue', 'yellow'],
-        ['yellow', 'yellow', 'red', 'blue', 'red']
-      ],
-      isRevealed: false
+      isRevealButtonOn: false
     }
     this.handleRevealClick = this.handleRevealClick.bind(this);
+    this.handleCardClick = this.handleCardClick.bind(this);
+    this.handleSkipClick = this.handleSkipClick.bind(this);
+    this.handleRestartClick = this.handleRestartClick.bind(this);
   }
 
   handleRevealClick() {
     this.setState({
-      isRevealed: !this.state.isRevealed
-    }, () => console.log(this.state.isRevealed))
+      isRevealButtonOn: !this.state.isRevealButtonOn
+    }, () => console.log(this.state.isRevealButtonOn))
+  }
+
+  handleSkipClick() {
+    this.setState({
+      isBluesTurn: !this.state.isBluesTurn
+    }, () => console.log(this.state.isBluesTurn))
+  }
+
+  handleRestartClick() {
+    console.log('shuffle a new board with new words and new colors and set isGuessed to false and set state to new board');
+  }
+
+  handleCardClick(indexRow, indexCol) {
+    console.log(indexRow, indexCol);
+    console.log(this.state.cards[indexRow][indexCol])
+    newCards = this.state.cards.slice();
+    newCards[indexRow][indexCol].isGuessed = true;
+    this.setState({
+      cards: newCards
+    }, () => console.log(this.state.cards));
   }
 
   render() {
@@ -83,10 +198,17 @@ class Game extends React.Component {
         <div>
           <h1>Codenames</h1>
         </div>
-        <Board words={this.state.words} isRevealed={this.state.isRevealed} colors={this.state.colors} />
+        <Board
+          isRevealButtonOn={this.state.isRevealButtonOn}
+          handleCardClick={this.handleCardClick}
+          cards={this.state.cards} />
         <InfoBoard
           isBluesTurn={this.state.isBluesTurn}
-          handleRevealClick={this.handleRevealClick} />
+          handleRevealClick={this.handleRevealClick}
+          handleSkipClick={this.handleSkipClick}
+          handleRestartClick={this.handleRestartClick}
+          cards={this.state.cards} />
+        <Rules />
       </div>
     )
   }
--- public/app.js
+++ public/app.js
@@ -16,17 +16,24 @@ var Card = function (_React$Component) {
   }
 
   _createClass(Card, [{
-    key: 'render',
+    key: "render",
     value: function render() {
+      var _this2 = this;
+
       var classes = 'card';
-      console.log('in card', this.props.isRevealed);
-      if (this.props.isRevealed) {
-        classes += ' ' + this.props.color + 'b';
+      if (this.props.isRevealButtonOn) {
+        classes += " " + this.props.card.color + "b";
+      }
+      if (this.props.card.isGuessed) {
+        classes += " " + this.props.card.color;
       }
       return React.createElement(
-        'td',
-        { className: classes },
-        this.props.value
+        "td",
+        { className: classes, onClick: function onClick(e) {
+            e.preventDefault();
+            _this2.props.handleCardClick(_this2.props.indexRow, _this2.props.indexCol);
+          } },
+        this.props.card.word
       );
     }
   }]);
@@ -44,25 +51,28 @@ var Board = function (_React$Component2) {
   }
 
   _createClass(Board, [{
-    key: 'render',
+    key: "render",
     value: function render() {
-      var _this3 = this;
+      var _this4 = this;
 
       return React.createElement(
-        'table',
-        { className: 'board' },
+        "table",
+        { className: "board" },
         React.createElement(
-          'tbody',
+          "tbody",
           null,
-          this.props.words.map(function (row, indexRow) {
+          this.props.cards.map(function (row, indexRow) {
             return React.createElement(
-              'tr',
+              "tr",
               { key: indexRow },
               row.map(function (card, indexCol) {
                 return React.createElement(Card, {
-                  value: card,
-                  key: indexRow * 6 + indexCol,
-                  isRevealed: _this3.props.isRevealed, color: _this3.props.colors[indexRow][indexCol] });
+                  card: card,
+                  key: indexRow * 5 + indexCol,
+                  isRevealButtonOn: _this4.props.isRevealButtonOn,
+                  indexRow: indexRow,
+                  indexCol: indexCol,
+                  handleCardClick: _this4.props.handleCardClick });
               })
             );
           })
@@ -84,28 +94,18 @@ var InfoBoard = function (_React$Component3) {
   }
 
   _createClass(InfoBoard, [{
-    key: 'render',
+    key: "render",
     value: function render() {
-      var _this5 = this;
-
       return React.createElement(
-        'div',
-        { className: 'infoboard' },
-        React.createElement(
-          'div',
-          { className: 'player' },
-          'Team ',
-          this.props.isBluesTurn ? 'Blue' : 'Red',
-          '\'s turn'
-        ),
-        React.createElement(
-          'button',
-          { className: 'revealbutton', onClick: function onClick(e) {
-              e.preventDefault();
-              _this5.props.handleRevealClick();
-            } },
-          'Reveal'
-        )
+        "div",
+        { className: "flexcontainer" },
+        React.createElement(Player, { team: "Blue" }),
+        React.createElement(Info, {
+          isBluesTurn: this.props.isBluesTurn,
+          handleRevealClick: this.props.handleRevealClick,
+          handleSkipClick: this.props.handleSkipClick,
+          handleRestartClick: this.props.handleRestartClick }),
+        React.createElement(Player, { team: "Red" })
       );
     }
   }]);
@@ -113,54 +113,273 @@ var InfoBoard = function (_React$Component3) {
   return InfoBoard;
 }(React.Component);
 
-var Game = function (_React$Component4) {
-  _inherits(Game, _React$Component4);
+var Player = function (_React$Component4) {
+  _inherits(Player, _React$Component4);
+
+  function Player() {
+    _classCallCheck(this, Player);
+
+    return _possibleConstructorReturn(this, (Player.__proto__ || Object.getPrototypeOf(Player)).apply(this, arguments));
+  }
+
+  _createClass(Player, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        { className: "scoreboard" },
+        React.createElement(
+          "h3",
+          null,
+          this.props.team,
+          " Team's Scores:"
+        ),
+        React.createElement(
+          "table",
+          null,
+          React.createElement(
+            "tbody",
+            null,
+            React.createElement(
+              "tr",
+              null,
+              React.createElement(
+                "td",
+                null,
+                "All cards:"
+              ),
+              React.createElement(
+                "td",
+                null,
+                "9"
+              )
+            ),
+            React.createElement(
+              "tr",
+              null,
+              React.createElement(
+                "td",
+                null,
+                "Remaining:"
+              ),
+              React.createElement(
+                "td",
+                null,
+                "4"
+              )
+            )
+          )
+        )
+      );
+    }
+  }]);
+
+  return Player;
+}(React.Component);
+
+var Info = function (_React$Component5) {
+  _inherits(Info, _React$Component5);
+
+  function Info() {
+    _classCallCheck(this, Info);
+
+    return _possibleConstructorReturn(this, (Info.__proto__ || Object.getPrototypeOf(Info)).apply(this, arguments));
+  }
+
+  _createClass(Info, [{
+    key: "render",
+    value: function render() {
+      var _this8 = this;
+
+      return React.createElement(
+        "div",
+        { className: "infoboard" },
+        React.createElement(
+          "h3",
+          { className: "playerturn" },
+          "Team ",
+          this.props.isBluesTurn ? 'Blue' : 'Red',
+          "'s turn"
+        ),
+        React.createElement(
+          "button",
+          { className: "skipbutton", onClick: function onClick(e) {
+              e.preventDefault();
+              _this8.props.handleSkipClick();
+            } },
+          "Skip"
+        ),
+        React.createElement("br", null),
+        React.createElement(
+          "button",
+          { className: "revealbutton", onClick: function onClick(e) {
+              e.preventDefault();
+              _this8.props.handleRevealClick();
+            } },
+          "Reveal"
+        ),
+        React.createElement("br", null),
+        React.createElement(
+          "button",
+          { className: "restartbutton", onClick: function onClick(e) {
+              e.preventDefault();
+              _this8.props.handleRestartClick();
+            } },
+          "Restart"
+        )
+      );
+    }
+  }]);
+
+  return Info;
+}(React.Component);
+
+var Rules = function (_React$Component6) {
+  _inherits(Rules, _React$Component6);
+
+  function Rules() {
+    _classCallCheck(this, Rules);
+
+    return _possibleConstructorReturn(this, (Rules.__proto__ || Object.getPrototypeOf(Rules)).apply(this, arguments));
+  }
+
+  _createClass(Rules, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        { className: "rules" },
+        React.createElement(
+          "h4",
+          null,
+          "Rules"
+        ),
+        React.createElement(
+          "p",
+          null,
+          "Codenames is a game of guessing which codenames (i.e., words) in a set are related to a hint-word given by another player.",
+          React.createElement("br", null),
+          React.createElement("br", null),
+          "Players split into two teams: red and blue. One player of each team is selected as the team's spymaster; the others are field operatives.",
+          React.createElement("br", null),
+          React.createElement("br", null),
+          "Twenty-five Codename cards, each bearing a word, are laid out in a 5\xD75 rectangular grid, in random order. A number of these words represent red agents, a number represent blue agents, one represents an assassin, and the others represent innocent bystanders.",
+          React.createElement("br", null),
+          React.createElement("br", null),
+          "The teams' spymasters are given a randomly-dealt map card showing a 5\xD75 grid of 25 squares of various colors, each corresponding to one of the code name cards on the table. Teams take turns. On each turn, the appropriate spymaster gives a verbal hint about the words on the respective cards. Each hint may only consist of one single word and a number. The spymaster gives a hint that is related to as many of the words on his/her own agents' cards as possible, but not to any others \u2013 lest they accidentally lead their team to choose a card representing an innocent bystander, an opposing agent, or the assassin.",
+          React.createElement("br", null),
+          React.createElement("br", null),
+          "The hint's word can be chosen freely, as long as it is not (and does not contain) any of the words on the code name cards still showing at that time. Code name cards are covered as guesses are made.",
+          React.createElement("br", null),
+          React.createElement("br", null),
+          "The hint's number tells the field operatives how many words in the grid are related to the word of the clue. It also determines the maximum number of guesses the field operatives may make on that turn, which is the hint's number plus one. Field operatives must make at least one guess per turn, risking a wrong guess and its consequences. They may also end their turn voluntarily at any point thereafter.",
+          React.createElement("br", null),
+          React.createElement("br", null),
+          "After a spymaster gives the hint with its word and number, their field operatives make guesses about which code name cards bear words related to the hint and point them out, one at a time. When a code name card is pointed out, the spymaster covers that card with an appropriate identity card \u2013 a blue agent card, a red agent card, an innocent bystander card, or the assassin card \u2013 as indicated on the spymasters' map of the grid. If the assassin is pointed out, the game ends immediately, with the team who identified him losing. If an agent of the other team is pointed out, the turn ends immediately, and that other team is also one agent closer to winning. If an innocent bystander is pointed out, the turn simply ends.",
+          React.createElement("br", null),
+          React.createElement("br", null),
+          "The game ends when all of one team's agents are identified (winning the game for that team),[3] or when one team has identified the assassin (losing the game). Source: ",
+          React.createElement(
+            "a",
+            { href: "https://en.wikipedia.org/wiki/Codenames_(board_game)" },
+            "Wikipedia"
+          )
+        )
+      );
+    }
+  }]);
+
+  return Rules;
+}(React.Component);
+
+var Game = function (_React$Component7) {
+  _inherits(Game, _React$Component7);
 
   function Game(props) {
     _classCallCheck(this, Game);
 
-    var _this6 = _possibleConstructorReturn(this, (Game.__proto__ || Object.getPrototypeOf(Game)).call(this, props));
+    var _this10 = _possibleConstructorReturn(this, (Game.__proto__ || Object.getPrototypeOf(Game)).call(this, props));
 
-    _this6.state = {
+    _this10.state = {
       isBluesTurn: true,
-      words: [['ball', 'fish', 'track', 'maple', 'pole'], ['sink', 'apple', 'spell', 'kid', 'cab'], ['lemon', 'scientist', 'whip', 'coffee', 'cat'], ['leaf', 'dance', 'princess', 'net', 'Europe'], ['water', 'yellow', 'bridge', 'watermelon', 'tea']],
-      colors: [['blue', 'blue', 'red', 'yellow', 'yellow'], ['red', 'black', 'blue', 'red', 'blue'], ['red', 'blue', 'red', 'yellow', 'yellow'], ['blue', 'blue', 'red', 'blue', 'yellow'], ['yellow', 'yellow', 'red', 'blue', 'red']],
-      isRevealed: false
+      cards: [[{ word: 'ball', color: 'blue', isGuessed: false }, { word: 'fish', color: 'blue', isGuessed: false }, { word: 'track', color: 'red', isGuessed: false }, { word: 'maple', color: 'yellow', isGuessed: false }, { word: 'pole', color: 'yellow', isGuessed: false }], [{ word: 'sink', color: 'red', isGuessed: false }, { word: 'apple', color: 'black', isGuessed: false }, { word: 'spell', color: 'blue', isGuessed: false }, { word: 'kid', color: 'red', isGuessed: false }, { word: 'cab', color: 'blue', isGuessed: false }], [{ word: 'lemon', color: 'red', isGuessed: false }, { word: 'scientist', color: 'blue', isGuessed: false }, { word: 'whip', color: 'red', isGuessed: false }, { word: 'coffee', color: 'yellow', isGuessed: false }, { word: 'cat', color: 'yellow', isGuessed: false }], [{ word: 'leaf', color: 'blue', isGuessed: false }, { word: 'dance', color: 'blue', isGuessed: false }, { word: 'princess', color: 'red', isGuessed: false }, { word: 'net', color: 'blue', isGuessed: false }, { word: 'Europe', color: 'yellow', isGuessed: false }], [{ word: 'water', color: 'yellow', isGuessed: false }, { word: 'yellow', color: 'yellow', isGuessed: false }, { word: 'bridge', color: 'red', isGuessed: false }, { word: 'watermelon', color: 'blue', isGuessed: false }, { word: 'tea', color: 'red', isGuessed: false }]],
+      isRevealButtonOn: false
     };
-    _this6.handleRevealClick = _this6.handleRevealClick.bind(_this6);
-    return _this6;
+    _this10.handleRevealClick = _this10.handleRevealClick.bind(_this10);
+    _this10.handleCardClick = _this10.handleCardClick.bind(_this10);
+    _this10.handleSkipClick = _this10.handleSkipClick.bind(_this10);
+    _this10.handleRestartClick = _this10.handleRestartClick.bind(_this10);
+    return _this10;
   }
 
   _createClass(Game, [{
-    key: 'handleRevealClick',
+    key: "handleRevealClick",
     value: function handleRevealClick() {
-      var _this7 = this;
+      var _this11 = this;
 
       this.setState({
-        isRevealed: !this.state.isRevealed
+        isRevealButtonOn: !this.state.isRevealButtonOn
       }, function () {
-        return console.log(_this7.state.isRevealed);
+        return console.log(_this11.state.isRevealButtonOn);
       });
     }
   }, {
-    key: 'render',
+    key: "handleSkipClick",
+    value: function handleSkipClick() {
+      var _this12 = this;
+
+      this.setState({
+        isBluesTurn: !this.state.isBluesTurn
+      }, function () {
+        return console.log(_this12.state.isBluesTurn);
+      });
+    }
+  }, {
+    key: "handleRestartClick",
+    value: function handleRestartClick() {
+      console.log('shuffle a new board with new words and new colors and set isGuessed to false and set state to new board');
+    }
+  }, {
+    key: "handleCardClick",
+    value: function handleCardClick(indexRow, indexCol) {
+      var _this13 = this;
+
+      console.log(indexRow, indexCol);
+      console.log(this.state.cards[indexRow][indexCol]);
+      newCards = this.state.cards.slice();
+      newCards[indexRow][indexCol].isGuessed = true;
+      this.setState({
+        cards: newCards
+      }, function () {
+        return console.log(_this13.state.cards);
+      });
+    }
+  }, {
+    key: "render",
     value: function render() {
       return React.createElement(
-        'div',
+        "div",
         null,
         React.createElement(
-          'div',
+          "div",
           null,
           React.createElement(
-            'h1',
+            "h1",
             null,
-            'Codenames'
+            "Codenames"
           )
         ),
-        React.createElement(Board, { words: this.state.words, isRevealed: this.state.isRevealed, colors: this.state.colors }),
+        React.createElement(Board, {
+          isRevealButtonOn: this.state.isRevealButtonOn,
+          handleCardClick: this.handleCardClick,
+          cards: this.state.cards }),
         React.createElement(InfoBoard, {
           isBluesTurn: this.state.isBluesTurn,
-          handleRevealClick: this.handleRevealClick })
+          handleRevealClick: this.handleRevealClick,
+          handleSkipClick: this.handleSkipClick,
+          handleRestartClick: this.handleRestartClick,
+          cards: this.state.cards }),
+        React.createElement(Rules, null)
       );
     }
   }]);
